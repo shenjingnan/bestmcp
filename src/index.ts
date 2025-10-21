9b2331e7e86f42e20541ba5c6b8f0095797f7921
shenjingnan <sjn.code@gmail.com>
2025-10-21 09:53:44 +0800

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export * from "../packages/core/src/decorators";
-export * from "../packages/core/src/errors";
-export * from "../packages/core/src/server";
-export * from "../packages/core/src/transports/index";
-export * from "../packages/core/src/validation";
-export * from "./types";
+export * from "@bestmcp/core";
