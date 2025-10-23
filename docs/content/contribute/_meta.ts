c025de42bfa33cb3179a24066520d5b5d0b1c7e9
shenjingnan <sjn.code@gmail.com>
2025-10-23 13:40:19 +0800

    WIP

---
+++ docs/content/contribute/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "dev-core": "开发框架",
+  "dev-example": "开发示例",
+};
+
+export default meta;
--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,10 +1,16 @@
 import nextra from "nextra";
 
-const withNextra = nextra({});
+const withNextra = nextra({
+  latex: true,
+  search: {
+    codeblocks: false,
+  },
+});
 
 export default withNextra({
   output: "export",
   images: {
     unoptimized: true,
   },
+  distDir: "out",
 });
