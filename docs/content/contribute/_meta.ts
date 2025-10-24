5f57cc72b751e4cd82d0ee7d18135035e60a8adb
shenjingnan <sjn.code@gmail.com>
2025-10-24 14:44:19 +0800

    WIP

--- docs/content/contribute/_meta.ts
+++ docs/content/contribute/_meta.ts
@@ -3,6 +3,8 @@ import type { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   "dev-core": "开发框架",
   "dev-example": "开发示例",
+  "why-use-alias-to-import-or-export": "为什么使用别名导入导出",
+  "why-separate-type-and-value-imports": "为什么分离类型和值导入",
 };
 
 export default meta;
