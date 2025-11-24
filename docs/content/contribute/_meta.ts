59264d82ee06171712abd587b2ae3869a1dd1b39
shenjingnan <sjn.code@gmail.com>
2025-11-24 14:22:40 +0800

    docs(contribute): 新增本地联动开发指南文档

--- docs/content/contribute/_meta.ts
+++ docs/content/contribute/_meta.ts
@@ -4,6 +4,7 @@ const meta: MetaRecord = {
   "getting-started": "贡献指南",
   "dev-core": "开发框架",
   "dev-example": "开发示例",
+  "local-development": "本地联动开发",
   "why-use-alias-to-import-or-export": "为什么使用别名导入导出",
   "why-separate-type-and-value-imports": "为什么分离类型和值导入",
   "release-process": "发布流程",
